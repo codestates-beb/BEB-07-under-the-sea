963f12c049a200d2aee2cb35dc64a140327cfdd6
HyunTae <113375908+hyuntae384@users.noreply.github.com>
2022-12-23 15:55:44 +0900

    개발환경세팅&라우터

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainPage() {
+  return <div>메인 페이지</div>;
+}
---
+++ client/src/pages/MyPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyPage() {
+  return <div>마이페이지</div>;
+}
