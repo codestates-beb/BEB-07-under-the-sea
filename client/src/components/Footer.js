26606fe4387f77494577021d9360fd1ad10e8740
yonnie-park <juon0805@gmail.com>
2022-12-24 23:11:27 +0900

    edit: header and footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,24 @@ import "./Footer.css"
 export default function Footer() {
   return(
     <div className="Footer">
-      <p>김현태, 박정연, 이동원, 백준석</p>
-      <br></br>
-      <a href="https://github.com/codestates-beb/BEB-07-under-the-sea">Github</a>
+      <div className="row">
+        <div className="col" id="col1">
+          <img src="./logo.png" alt="logo" id="logo"></img>
+          <br></br>
+          <h3>Under the Sea</h3><br></br>
+          <p id="description">The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.</p>
+          <br></br>
+        </div>
+        <div className="col" id="col2">
+          <h5>박정연</h5>
+          <h5>김현태</h5> 
+          <h5>이동원</h5>
+          <h5>백준석</h5>
+        </div>
+        <div className="col">
+        <a id="col4" href="https://github.com/codestates-beb/BEB-07-under-the-sea">Github</a>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { ReactDOM } from "react";
 import { useState } from "react";
 import "./Header.css"
 
