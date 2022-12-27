0ef9c63fd34ed1c7a38e3becf09c035a0e55d82c
yonnie-park <juon0805@gmail.com>
2022-12-27 16:30:46 +0900

    edit: component

--- client/src/components/NFTitem.js
+++ client/src/components/NFTitem.js
@@ -4,14 +4,22 @@ import json from "../resources/dummyNFT";
 
 function NFTitem() {
     return (
-        <div className="items">
-            <div id="image">
-              <img src={json.imgUrl} alt="sample" id="thumbnail"></img>
-            </div>
-            <div id="name"><h4>{json.name}</h4></div>
-            <div id="collection"><h5>{json.collection}</h5></div>
-            <div id="price"><p>{json.price}</p></div>
+        <div className="row">
+            {json.map((el)=>{
+            return(
+                <div className="items">
+                    <div id="image">
+                        <img src={el.imgUrl} alt="sample" id="thumbnail"></img>
+                    </div>
+                    <div id="name"><h4>{el.name}</h4></div>
+                    <div id="collection"><h5>{el.collection}</h5></div>
+                    <div id="price"><p>{el.price}</p></div>
+                </div>
+            )
+        })}
         </div>
+        
+            
     );
 };
 
