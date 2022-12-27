7fed3ec632e5d248c4a57fb6ed2a50d69a982433
yonnie-park <juon0805@gmail.com>
2022-12-27 17:43:13 +0900

    edit: nft component

--- client/src/components/created.js
+++ client/src/components/created.js
@@ -9,7 +9,7 @@ function Created() {
   const [filteredNFT, setFilteredNFT] = useState(json)
 
   const handleChange = (event) => {
-    setSearchNFT(event.value.target)
+    setSearchNFT(event.target.value)
   }
 
   const handleFilteredNFT = () => {
