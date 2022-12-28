5c59dcb354e6d3708fc1ba99f93eeb2f508212fb
yonnie-park <juon0805@gmail.com>
2022-12-28 14:44:13 +0900

    edit: nft component

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -7,7 +7,7 @@ import data from "../resources/dummyNFT";
 export default function MainPage() {
   return (
     <div className="mainpage">
-      <div className="row">
+      <div id="row">
         <div className="intro">
             <h1>Explore, collect, and sell NFTs</h1>
             <h3>World's first and largest NFT marketplace</h3>
@@ -17,7 +17,7 @@ export default function MainPage() {
             </div>
           </div>
         </div>
-        <div className="row">
+        <div id="row2">
           {data.map((e)=> (
             <Item imgUrl={e.imgUrl} name={e.name} collection={e.collection} price={e.price}/>
           ))}
