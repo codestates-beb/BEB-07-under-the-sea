63065f6217864dc98d6a7cff6d187bc2dcf927bc
fromdwlee <desgod@naver.com>
2022-12-28 11:01:59 +0900

    add: 스마트 컨트랙트 abi, tokenlist component, mintpage minting 함수 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from "react";
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 import MintPage from './pages/MintPage'
@@ -9,13 +10,31 @@ import Footer from './components/Footer';
 import DetailsPage from './pages/DetailsPage';
 
 function App() {
+  const [account, setAccount] = useState("");
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const account = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(account[0]);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, [account]);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/mypage/:account' element={<MyPage />} />
-        <Route path='/mintpage' element={<MintPage />} />
+        <Route path='/mintpage' element={<MintPage account={account} />} />
         <Route path='/marketplace' element={<MarketPlacePage />} />
         <Route path='/details' element={<DetailsPage />} />
       </Routes>
