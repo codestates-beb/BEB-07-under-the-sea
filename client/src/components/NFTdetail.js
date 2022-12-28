9f4c97dfa5c238845f64ddb27b3d785cf7ec0960
yonnie-park <juon0805@gmail.com>
2022-12-28 17:26:06 +0900

    add: details page props

--- client/src/components/NFTdetail.js
+++ client/src/components/NFTdetail.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./NFTdetail.css";
 
 const NFTdetail = (props) => {
-    
     return(
             <div id="detailContainer">
                 <img src={props.imgUrl} id="detailThumbnail" alt="item img"></img>
-                <h4 id="detailCollection">{props.collection}</h4>
+                <div id="detailContents">
+<h4 id="detailCollection">{props.collection}</h4>
                 <h4 id="detailName">{props.name}</h4>
                 <p id="detailPrice">{props.price}</p>
                 <p id="detailDescription">{props.description}</p>
+                </div>
+                
             </div>
         
     )
