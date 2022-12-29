bb43516a71ecb2a519aff888c00bad0235a4f91a
yonnie-park <juon0805@gmail.com>
2022-12-29 20:06:08 +0900

    add: send NFT button

--- client/src/components/NFTdetail.js
+++ client/src/components/NFTdetail.js
@@ -10,8 +10,7 @@ const NFTdetail = (props) => {
                     <h4 id="detailName">{`NFT name #` + props.id}</h4>
                     <p id="detailPrice">0.1eth</p>
                     <p id="detailDescription">We like to own stuff. We especially like to own things that we find valuable, whether from an emotional or financial standpoint. Throughout history, we’ve sought ownership of physical objects—there’s a certain charm associated with the real-life qualities of the things we own. </p>
-                    <button id="purchaseBTN">Purchase NFT</button>
-                    <button id="purchaseBTN">Sell NFT</button>
+                    <button id="purchaseBTN">NFT 선물하기</button>
                 </div>
                 
             </div>
