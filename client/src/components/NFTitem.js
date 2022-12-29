8bf464265ffac0807b9c23a40956cad3a6f6d8d4
yonnie-park <juon0805@gmail.com>
2022-12-29 10:59:08 +0900

    add:main page nft

--- client/src/components/NFTitem.js
+++ client/src/components/NFTitem.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NFTitem.css";
 
-const NFTitem = (props) => {
+const NFTitem = (erc721list) => {
     return(
-        <Link to={`/details/${props.id}`} id="NFTlink">
+        <Link to={`/details/${erc721list.tokenId}`} id="NFTlink">
             <div id="itemContainer">
-                <img src={props.imgUrl} id="thumbnail" alt="item img"></img>
-                <h4 id="NFTcollection">{props.collection}</h4>
-                <h4 id="NFTname">{props.name}</h4>
-                <p id="NFTprice">{props.price}</p>
+                <img src={erc721list.tokenURI} id="thumbnail" alt="item img"></img>
+                <h4 id="NFTcollection">{erc721list.collection}</h4>
+                <h4 id="NFTname">{erc721list.name}</h4>
+                <p id="NFTprice">{erc721list.price}</p>
             </div>
         </Link>
         
