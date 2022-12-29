7aa3524a0a453cf0c7ca41ba5b26d3510f9e7c10
yonnie-park <juon0805@gmail.com>
2022-12-29 17:48:10 +0900

    add: loading

--- client/src/pages/DetailsPage.js
+++ client/src/pages/DetailsPage.js
@@ -3,10 +3,10 @@ import { tokenContract } from "../erc721Abi";
 import {useParams} from "react-router-dom"
 import "./DetailsPage.css"
 import NFTdetail from "../components/NFTdetail"
+import LoadingIndicator from "../components/LoadingIndicator";
 
 const DetailsPage = ()=>{
     const [erc721list, setErc721list] = useState([]);
-
   const getErc721Token = async () => {
 
     const name = await tokenContract.methods.name().call();
@@ -26,6 +26,7 @@ const DetailsPage = ()=>{
           .tokenURI(tokenId)
           .call();
         setErc721list((prevState) => {
+            
           return [...prevState, { name, symbol, tokenId, tokenURI }];
         });
       }
@@ -37,7 +38,9 @@ const DetailsPage = ()=>{
 
     useEffect(() => {
       if (didMount.current) func();
-      else didMount.current = true;
+      else{
+        didMount.current = true
+      }
     }, deps);
   };
 
@@ -49,6 +52,9 @@ const DetailsPage = ()=>{
     let filteredNFT = erc721list.filter((e) => e.tokenId===id)
     return(
         <div id="detailsContainer">
+            <div id="loading">
+                <LoadingIndicator/>
+            </div>
             {filteredNFT.map((el)=>{
                 return <NFTdetail imgUrl={el.tokenURI} name={el.name} collection={el.collection} price={el.price} id={el.tokenId} description={el.description}/>
             })}
